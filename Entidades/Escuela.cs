508abea5379e02621faf3ab1086e534e9ef14624
César Arévalo <cesarevalo1995@gmail.com>
2021-06-18 10:59:11 -0500

    avance etapa 3

--- Entidades/Escuela.cs
+++ Entidades/Escuela.cs
@@ -1,13 +1,15 @@
+using System.Collections.Generic;
+
 namespace CoreEscuela.Entidades{
 
     class Escuela
     {
-
         //Formas de crear variables y de encapsular
             //forma 1
+        string nombre;  
         public  string Nombre{
-            get{return "Copia" + Nombre;}
-            set{Nombre = value;}
+            get{return "Copia " + nombre;}
+            set{nombre = value.ToUpper();}
         }
             //forma 2
         public int AnoCreacion{get; set;}
@@ -15,6 +17,10 @@ namespace CoreEscuela.Entidades{
 
         //prop atajo para crear una propiedad
         public string Ciudad{get;set;}
+
+        public List<Curso> Cursos { get; set; }
+        
+        public TiposEscuela TipoEscuela { get; set; }
 
         //forma 1 de escribir constructores
         public Escuela (string nombre, int ano){
@@ -24,7 +30,19 @@ namespace CoreEscuela.Entidades{
 
         //forma 2 de escribir constructores
         public Escuela(string nombre, int ano, string pais) => (Nombre, AnoCreacion, Pais) = (nombre, ano, pais);
+        public Escuela(string nombre, int ano,
+                         TiposEscuela tipo, 
+                         string pais = "",string ciudad = "" ) 
+                         {
+                            (Nombre, AnoCreacion, Pais) = (nombre, ano, pais); 
+                            Pais = pais;
+                            Ciudad = ciudad;
+                         }
 
+        public override string ToString()
+        {
+            return $"Nombre: \"{Nombre}\", Tipo: {TipoEscuela} {System.Environment.NewLine} Pais: {Pais}, Ciudad: {Ciudad}"; 
+        }
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,17 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
 using CoreEscuela.Entidades;
+using static System.Console;
 
 namespace CursoBasicoDotNet
 {
-    class Programs
+    class Program
     {
         static void Main(string[] args)
-        {   
-            var escuela = new Escuela("Platzi Academy",2012);
-            escuela.Pais = "Colombia";
-            escuela.Ciudad = "Bogotá";
+        {
+            var escuela = new Escuela("Platzi Academy", 2012, TiposEscuela.Primaria,
+                pais: "Colombia", ciudad: "Bogota"
+            );
 
-            Console.WriteLine(escuela.AnoCreacion);
+            escuela.Cursos = new List<Curso> {
+                new Curso(){Nombre = "101", Jornada = TiposJornada.Mañana},
+                new Curso(){Nombre = "201", Jornada = TiposJornada.Mañana},
+                new Curso(){Nombre = "301", Jornada = TiposJornada.Mañana}
+            };
+
+            escuela.Cursos.Add(new Curso { Nombre = "102", Jornada = TiposJornada.Tarde });
+            escuela.Cursos.Add(new Curso { Nombre = "202", Jornada = TiposJornada.Tarde });
+
+            var otraCollecion = new List<Curso> {
+                new Curso(){Nombre = "401", Jornada = TiposJornada.Mañana},
+                new Curso(){Nombre = "501", Jornada = TiposJornada.Mañana},
+                new Curso(){Nombre = "501", Jornada = TiposJornada.Tarde}
+            };
+            otraCollecion.REM ();
+            escuela.Cursos.AddRange(otraCollecion);
+
+
+           /*  escuela.Cursos = new Curso[] {
+                new Curso(){Nombre = "101"},
+                new Curso(){Nombre = "201"},
+                new Curso(){Nombre = "301"}
+            }; */
+
+            WriteLine(escuela);
+            WriteLine("==============");
+
+            WriteLine("==============");
+            ImprimirCursosEscuelaForEach(escuela.Cursos);
+
+
+        }
+
+        private static void ImprimirCursosEscuelaForEach(List<Curso> arregloCursos)
+        {
+            foreach (var item in arregloCursos)
+            {
+                Console.WriteLine($"Nombre{item.Nombre}, Id {item.UniqueId}");
+            }
         }
     }
 }
+
